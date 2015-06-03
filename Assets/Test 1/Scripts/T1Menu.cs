9695c91a852ccb75b53bb985b767e22666f92d82
evgeniy.yudenko <evgeniy.yudenko@adecosystems.com>
2015-06-04 02:02:05 +0300

    Start

---
+++ Assets/Test 1/Scripts/T1Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class T1Menu : MonoBehaviour {
+	public Text ip;
+	public Text port;
+	public Text countClients;
+	private NetworkView netView;
+
+	public void Start () {
+		netView = new NetworkView ();
+	}
+
+	public void MyStartServer () {
+		Network.InitializeServer (int.Parse (countClients.text), int.Parse (port.text), true);
+	}
+
+	public void MyStopServer () {
+		MasterServer.UnregisterHost ();
+		Network.DestroyPlayerObjects (Network.connections[0]);
+	}
+	
+	public void MyConnectToServer () {
+		
+	}
+
+	public void MyDisconnect () {
+
+	}
+}
